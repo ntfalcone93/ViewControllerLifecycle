a079137d27b1de6c71057aeefb50906f09a569c8
Nathan Falcone <ntfalcone93@gmail.com>
2018-03-14 10:32:38 -0600

    Setup FirstViewController

--- ViewLifecycle/FirstViewController.swift
+++ ViewLifecycle/FirstViewController.swift
@@ -8,40 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var blueButton: UIButton!
     
     // Gets called when the view is loaded into memory
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("View did load")
+        print("View did load \n")
+        
+        // Design view
     }
     
     // Gets called when the view is about to appear on the screen
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("View will appear")
+        print("View will appear \n")
+        
+        // Update view
     }
     
     // Gets called when the view does appear on the screen
     override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print("View did appear")
+        print("View did appear \n")
+        
+        // Animate view
     }
     
     // Gets called when the view is about to be removed from the screen
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        print("View will disappear")
+        print("View will disappear \n")
+        
+        // Animate disappearance
     }
     
     // Gets called when the view is removed from the screen
     override func viewDidDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        print("View did disappear")
+        print("View did disappear \n")
+        
+        // Do something when the view has disappeared
     }
 }
